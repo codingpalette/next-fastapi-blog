c2f0b9861fdf520985b962c5d278b8fc61ba87f0
codingpalette <codingpalette@gmail.com>
2022-06-25 21:21:05 +0900

    사이드바 컴포넌트 수정

--- front/components/base/SideBar/styles.ts
+++ front/components/base/SideBar/styles.ts
@@ -1,9 +1,16 @@
 import styled from '@emotion/styled'
 import { css } from '@emotion/react';
 import transitions from "../../../lib/transitions";
+import {palette} from "../../../lib/globalStyles";
 
 export const SideBarNavBox = styled.div<{ active: boolean }>`
   width: 250px;
+  background-color: #D8D2B9;
+  .title{
+    background-color: ${palette.backActive};
+    color: ${palette.textActive};
+  }
+  
   ${(props) => props.active 
     ? css`
       animation: ${transitions.sideInFromRight} 0.2s forwards ease-in-out;
@@ -11,5 +18,9 @@ export const SideBarNavBox = styled.div<{ active: boolean }>`
     : css`
       animation: ${transitions.sideOutToRight} 0.2s forwards ease-in-out;
     ` 
+  }
+  
+  nav{
+    //background-color: ${palette.back};
   }
 `

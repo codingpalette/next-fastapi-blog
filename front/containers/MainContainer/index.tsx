74fe5a3721f4c30a9c60991fa3ed9ba785652cfe
codingpalette <codingpalette@gmail.com>
2022-07-03 13:43:00 +0900

    main container 수정

--- front/containers/MainContainer/index.tsx
+++ front/containers/MainContainer/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Header from "../../components/base/Header";
 import {ContentBox, Pattern} from "./styles";
-import {useQuery, useQueryClient} from "react-query";
-import fetcher from "../../lib/fetcher";
-import axios from "axios";
+
 
 interface MainContainerProps {
   children: React.ReactNode;
@@ -11,42 +9,6 @@ interface MainContainerProps {
 
 
 const MainContainer = ({children}: MainContainerProps) => {
-  const queryClient = useQueryClient()
-
-  const {
-    isLoading: userLoading,
-    isError: userIsError,
-    data: userData,
-    error: userError,
-  } = useQuery("user_check", () => fetcher("/api/user/check"), {
-    onSuccess: (data) => {
-      if (data) {
-        tokenRefresh()
-      }
-    },
-    onError: (e) => {
-      queryClient.setQueryData("user_check", undefined)
-    },
-  })
-
-  const tokenRefresh = async () => {
-    try {
-      await fetcher(`/api/user/token/refresh`)
-      onLoginSuccess()
-    } catch (e) {
-      await axios.post("/api/user/logout")
-      queryClient.setQueryData("user_check", undefined)
-    }
-  }
-
-  const onLoginSuccess = () => {
-    const JWT_EXPIRY_TIME = 24 * 3600 * 1000 // 만료 시간 (24시간 밀리 초로 표현)
-    setTimeout(() => {
-      tokenRefresh()
-    }, JWT_EXPIRY_TIME - 60000)
-  }
-
-
   return(
     <>
       <div className="w-full h-full">
